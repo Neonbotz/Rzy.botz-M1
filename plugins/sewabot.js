2451d1fc0d43d26961c660b1c1c2e43b79a7e652
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-01 17:39:24 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,17 +11,17 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085828764046 
-GoPay: 085828764046
-Dana: 085828764046
+Pulsa : 087719424487
+GoPay: -
+Dana: -
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp15.000\nSewa bot tanpa batasan waktu.", rowId:".owner"},
-        {title: '1 Bulan', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".owner"},
-        {title: '1 Minggu', description: "Rp7.000\nSewa bot selama 1 minggu.", rowId:".owner"},
+        {title: '1 Tahun', description: "Rp50.000\nSewa bot tanpa batasan waktu.", rowId:".owner"},
+        {title: '1 Bulan', description: "Rp20.000\nSewa bot selama 1 bulan.", rowId:".owner"},
+        {title: '1 Minggu', description: "Rp10.000\nSewa bot selama 1 minggu.", rowId:".owner"},
         {title: 'Trial', description: "GRATIS\nBot gratis 3 Hari.", rowId:".join"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
