a266904b4465a4074c4105981cf5308aa05030d0
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-01 17:48:45 +0700

    Update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur dari Rzy-bitz saat ini: ${totalf}`,m)
+conn.reply(m.chat, `Total Fitur dari Sachi-Botzzえ saat ini: ${totalf}`,m)
 }
 
 handler.help = ['totalfitur']
