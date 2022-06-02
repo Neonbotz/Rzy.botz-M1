da443ba376b016c11d1af0f5636d12d2da85d941
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-02 16:13:49 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -15,8 +15,8 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'Rzy-botz tidak aktif' : banned ? 'kamu dibanned' : 'Rzy-botz disini',
-                '©Rzy-botz',
+                isBanned ? 'Sachi-Botzzえ tidak aktif' : banned ? 'kamu dibanned' : 'Sachi-Botzz Disini Kak <3',
+                '© Sachi-Botzzえ',
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
                 m.isGroup ? 'Ban' : isBanned ? 'Unban' : 'Donasi',
@@ -29,9 +29,9 @@ handler.all = async function (m, { isBlocked }) {
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
         this.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 10000*
-╠➥ *Permanen* : *Rp 15000*
-╠➥ *Premium* :   *Rp 15000*
+╠➥ *1 Bulan* :      *Rp 20k*
+╠➥ *Permanen* : *Rp Tidak Tersedia*
+╠➥ *Premium* :   *Rp 15k*
 ╠➥ *Sc Bot* :        *Masih Beta*
 ║
 ╠═〘 PEMBAYARAN 〙 ═
@@ -41,8 +41,8 @@ handler.all = async function (m, { isBlocked }) {
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
 ╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 Rzy-botz 〙 ═`.trim(), '© Rzybotz', 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
+╠═ Scrip original by Narutono
+╠═〘 Rzy-botz 〙 ═`.trim(), '© Sachi-Botzzえ', 'Dana', '#viadana', 'Pulsa', '#viapulsa', m)
 }
 
     // salam
@@ -72,7 +72,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`im Rzy-botz | ⏰Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | by Rzy-botz`).catch(_ => _)
+        await this.setStatus(`im Sachi-Botzzえ | ⏰Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | by Sachi-Botzzえ`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
